9892c98cc4257a8c71b06cc89d94c63060ad9b17
Worawit Penglam <worawit.pen@student.mahidol.edu>
2023-01-17 00:06:17 +0700

    add Information for Machine table

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/VendingMachine'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+db = SQLAlchemy(app)
 
 
-if __name__ == '__main__':
-    app.run()
+class Machine(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+
+
+
+
+
+# @app.route('/')
+# def hello_world():  # put application's code here
+#     return 'Hello World!'
+#
+#
+# if __name__ == '__main__':
+#     app.run()
